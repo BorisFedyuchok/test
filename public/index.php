13b0537917ca463431e20249cc23f5e8ef1573ad
Boris1310 <novyjpolzovatel@MacBook-Pro-Boris.local>
2021-09-28 22:45:54 +0300

    test

---
+++ public/index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>MySite</title>
+</head>
+<body>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="index.html" class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
+                <img src="../../../../../Users/Boris/Desktop/ls/img/logo.png" width="75" alt="technoprod logo">
+            </a>
+
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-white">Главная</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">Список товаров</a></li>
+
+                <li><a href="#" class="nav-link px-2 text-white">О Нас</a></li>
+            </ul>
+
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+            </form>
+
+            <div class="text-end">
+                <a href="#"><button type="button" class="btn btn-outline-light me-2">Личный кабинет</button></a>
+                <a href="#"><button type="button" class="btn btn-warning">Корзина</button></a>
+            </div>
+        </div>
+    </div>
+</header>
+<main class="container">
+    <?php
+
+
+
+    ?>
+    ?>
+</main>
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Главная</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Наши товары</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Категории</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Скидки</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">О Нас</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2021 Company, Inc</p>
+</footer>
+<script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
+</html>
